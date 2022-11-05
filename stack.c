604c5e84eee03e55634f1a7fd2049e11ec6b3ed6
Saptarshi  Paul <ohm@Saptarshis-MacBook-Pro.local>
2022-11-05 18:48:19 -0400

    + pushing stack.c

---
+++ stack.c
@@ -0,0 +1,44 @@
+
+// implement stack
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void push(int);
+int pop();
+void _display();
+
+int *arr; // ponter to global array.
+
+int main() {
+
+    // implement dynamic array here.
+    // Test cases.
+    push(3);
+    push(5);
+    pop(); // output => "5 was popped out from the array at index 1";
+    push(6);
+    push(7);
+    push(8);
+    pop() //  output => "8 was popped out from the array at index 3";
+    _display(); // Expected output => 3, 5, 6, 7
+}
+
+// Implement push
+void push(int data) {
+
+    // TODO
+}
+
+// Implement pop
+int pop() {
+
+//TODO
+
+return pop_value;
+}
+
+// Implemetn display
+void display() {
+
+}
