07f616bf3522a440c454de082d888c647313aab6
AnshiRud <anshi.rudraraju@gmail.com>
2022-11-23 20:38:37 -0500

    small changes

--- stack.c
+++ stack.c
@@ -7,12 +7,13 @@
 void push(int);
 int pop();
 void _display();
-
+//allows push pop to be used
 int *arr; // ponter to global array.
-
+int i = 0;
 int main() {
-
     // implement dynamic array here.
+    arr = (int*) malloc (sizeof(int) * 5);
+
     // Test cases.
     push(3);
     push(5);
@@ -20,25 +21,39 @@ int main() {
     push(6);
     push(7);
     push(8);
-    pop() //  output => "8 was popped out from the array at index 3";
+    pop(); //  output => "8 was popped out from the array at index 3";
     _display(); // Expected output => 3, 5, 6, 7
 }
 
 // Implement push
 void push(int data) {
-
+     arr[i] = data;
+     i++;
     // TODO
 }
 
 // Implement pop
-int pop() {
+int pop() { 
+int pop_value;
+if(i<0)
+{
+    printf ("error");
+    return -1;
+}
+else
+{
+    i = i-1;
+    pop_value = arr[i];
+    return pop_value; 
+}
 
-//TODO
-
-return pop_value;
 }
 
 // Implemetn display
-void display() {
+void _display() {
+    int j;
+    for(j=0; j<i; j++) {
+        printf("%d\n", arr[j]);
+    }
 
 }
