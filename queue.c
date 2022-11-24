4a319543781dcb84a29b71d2ddbc8b1e630e0d02
Saptarshi  Paul <ohm@Saptarshis-MacBook-Pro.local>
2022-11-23 20:19:43 -0500

    + added queue

---
+++ queue.c
@@ -0,0 +1,21 @@
+// Implement queue
+
+int main() {
+    // dynamic array
+
+    insert(5);
+    insert(6);
+    delete();
+    display();
+
+    return 0;
+}
+
+void insert() {
+    // TODO
+
+}
+
+void delete() {
+    // TODO
+}
